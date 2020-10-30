07d7efceca569e219db8bd07ab11b6bd4b568771
rmccabe62 <72869399+rmccabe62@users.noreply.github.com>
2020-10-30 14:04:38 -0400

    updated files

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Hello, World");
+document.write('Hello, World');
+var A = "\"I told you not to tell him," + "you said I\'m going to anyway\""
+document.write(A);
+document.write("\"I told him not to do that you will get in trouble");
+document.write("\"I told you not to tell him," + "you said I\'m going to anyway\"");
+var Students = "Computer Science", Student1 = "John", Student2 = "Betty", Student3 = "Peter",
+                Student4 = "Sally";
+document.write(Student2);
