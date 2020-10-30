42d341d4e1507e89c10b2e81b29773a38bf224fb
rmccabe62 <72869399+rmccabe62@users.noreply.github.com>
2020-10-30 17:46:45 -0400

    update files

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,9 +1,22 @@
-window.alert("Hello, World");
-document.write('Hello, World');
-var A = "\"I told you not to tell him," + "you said I\'m going to anyway\""
-document.write(A);
-document.write("\"I told him not to do that you will get in trouble");
-document.write("\"I told you not to tell him," + "you said I\'m going to anyway\"");
+window.alert("Hello, World");   //Prompting a pop up box with a message to a user.
+document.write('Hello, World'); //Writing text to  the screen.
+var A = "\"I told you not to tell him," + "you said I\'m going to anyway\"" //Creating a string variable.
+document.write(A);  //Write the variable to the screen
+document.write("\"I told him not to do that you will get in trouble"); //Writing a string to the screen
+document.write("\"I told you not to tell him," + "you said I\'m going to anyway\"");    //Concantinating two strings and writing them to the screen
 var Students = "Computer Science", Student1 = "John", Student2 = "Betty", Student3 = "Peter",
-                Student4 = "Sally";
-document.write(Student2);
+                Student4 = "Sally";     //Declaring multiple string variables and assigning values to them
+document.write(Student2);       //Choosing a single string variable and writing it to the screen
+var Student1 = "John\'s font color is red";     //Declaring first variable and assigning a value to it
+var Student1=Student1.fontcolor("red");     //Assigning a font color to the string variable
+document.write(Student1);       //Writing the string variable with it's font color to the screen
+var Student2 = "Betty\'s font color is green";      //Declaring second variable and assigning a value to it
+var Student2 = Student2.fontcolor("green");         //Assigning a font color to the second variable
+document.write(Student2);                           //Writing the variable along with it's color to the screen 
+var Student3 = "Peter\'s font color is blue";       //Declaring a third variable and assigning a value to it
+var Student3 = Student3.fontcolor("blue");          //Assigning a font color to the variable
+document.write(Student3);                           //Writing the variable along with it's color to the screen
+var Student4 = "Sally\'s font color is red";        //Declaring a fourth variable and assigning a value to it
+var Student4 = Student4.fontcolor("red");           //Assigning a font color to the variable
+document.write(Student4);                           //Writing the variable along with it's color to the screen
+document.write(5 + 5);                              //Passing two arguments to be added together and writing the result to the screen
