44fbc3219a50a0460decce9f22ad8ecf11c7bfca
rmccabe62 <72869399+rmccabe62@users.noreply.github.com>
2020-10-31 15:55:51 -0400

    updated files not working

---
+++ Basic JavaScript Projects/Project2_functions/JS/add_two.js
@@ -0,0 +1,14 @@
+var z = addTwo(4,5);
+document.getElementById("added").innerHTML = z;
+
+
+function addTwo(X,Y)   {
+    return   X + Y;     //The function returns the sum of X and Y
+    
+}
+
+
+
+
+
+
