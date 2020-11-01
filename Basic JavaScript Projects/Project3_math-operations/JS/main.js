5f5b0417586272f989428672e3dc1aa22113b14d
rmccabe62 <72869399+rmccabe62@users.noreply.github.com>
2020-11-01 16:26:05 -0500

    updated files

--- Basic JavaScript Projects/Project3_math-operations/JS/main.js
+++ Basic JavaScript Projects/Project3_math-operations/JS/main.js
@@ -11,5 +11,19 @@ function minusTwo()     {
     document.getElementById("math").innerHTML = "8 - 3 =" + minustime;
 }
 
+function multiplication()       {
+    var simple_math = 8 * 7;
+    document.getElementById("math").innerHTML = "8 * 7 =" + simple_math;
+}
+
+function division()     {
+    var simple_math = 9 / 3;
+    document.getElementById("math").innerHTML = "9 / 3 =" + simple_math;
+}
+
+function mathExplosion()    {
+    var all_math = (3 + 5) * 8 / 2 -3;
+    document.getElementById("math").innerHTML = "3 plus 5 multiplied by 8 divided in half minus 3 equals " + all_math;
+}
     
 
