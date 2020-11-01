356503450007fdd696c8c33b93d9a2886823a7b4
rmccabe62 <72869399+rmccabe62@users.noreply.github.com>
2020-11-01 09:14:35 -0500

    update files

--- Basic JavaScript Projects/Project2_functions/JS/add_two.js
+++ Basic JavaScript Projects/Project2_functions/JS/add_two.js
@@ -1,10 +1,6 @@
-var z = addTwo(4,5);
-document.getElementById("added").innerHTML = z;
-
-
 function addTwo(X,Y)   {
-    return   X + Y;     //The function returns the sum of X and Y
-    
+     var z = X + Y;
+    document.getElementById("added").innerHTML = z;
 }
 
 
---
+++ Basic JavaScript Projects/Project3_math-operations/JS/main.js
@@ -0,0 +1,15 @@
+function addingTwo()     {
+    var first_operand = 10;
+    var second_operand = 15;
+    var X = first_operand + second_operand;
+    document.getElementById("math").innerHTML = X;
+    
+}
+
+function minusTwo()     {
+    var minustime = 8-3;
+    document.getElementById("math").innerHTML = "8 - 3 =" + minustime;
+}
+
+    
+
