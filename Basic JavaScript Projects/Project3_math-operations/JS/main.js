b2fe36ddd1824dff4bb62dcf31523f40bf934527
rmccabe62 <72869399+rmccabe62@users.noreply.github.com>
2020-11-02 09:11:00 -0500

    updated files

--- Basic JavaScript Projects/Project3_math-operations/JS/main.js
+++ Basic JavaScript Projects/Project3_math-operations/JS/main.js
@@ -24,6 +24,28 @@ function division()     {
 function mathExplosion()    {
     var all_math = (3 + 5) * 8 / 2 -3;
     document.getElementById("math").innerHTML = "3 plus 5 multiplied by 8 divided in half minus 3 equals " + all_math;
-}
-    
 
+}
+
+function modOp()    {
+    var remaindMath = 8 % 3;
+    document.getElementById("math").innerHTML = "When you divide 8 by 3 your remainder is " +remaindMath;
+}
+
+function negation_Operation()   {
+    var x = 14
+    document.getElementById("math").innerHTML = -x;
+}
+
+function incrementNumber()      {
+    var x = 9;
+    x++;
+    document.write(x);
+}
+
+function decrementNumber()   {
+    var y = 11;
+    y--;
+    document.write(y);
+
+}
