e0b2adbcdb9456d96d4863a8291b8f3fbcb3d01a
rmccabe62 <72869399+rmccabe62@users.noreply.github.com>
2020-11-04 13:52:26 -0500

    updated files

--- Basic JavaScript Projects/Project6_ternery__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternery__operators_constructors/JS/main.js
@@ -1,6 +1,6 @@
 function Ride_Function()    {
     var Height, can_Ride;
-    Height = document.getElementById("Height").innerHTML.value;
+    Height = document.getElementById("Height").value;
     can_Ride =(Height < 52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML = can_Ride + " to ride."; 
 }
@@ -18,24 +18,34 @@ function student_Info()     {
 } 
 
 function reserved_Word()    {
-    var else = "D";
-    document.write("else");
+    var D = Math;
+    document.write("D");
 }
 
 //Constuctor function for new customer
 function customer_Info(Firstname, Lastname, Email, Phonenumber)     {
-    this.firstname = first_name;    
-    this.lastname = last_name;
-    this.email = email_Add;
-    this.phonenumber = phone_Num;
+    this.firstname = Firstname;    
+    this.lastname = Lastname;
+    this.email = Email;
+    this.phonenumber = Phonenumber;
 
 }
 
 //Create new customer objects
-var Cust1 = new Customer("Bill", "Jones", "william.jones@myemail.com", "234-975-1135");
-var Cust2 = new Customer("Sally", "Smith", "sallysmith@jj.me", "890-431-9256");
-var Cust3 = new Customer("John", "Johnson", "jjohn@me.org", "657-902-3368");
+var Cust1 = new customer_Info("Bill", "Jones", "william.jones@myemail.com", "234-975-1135");
+var Cust2 = new customer_Info("Sally", "Smith", "sallysmith@jj.me", "890-431-9256");
+var Cust3 = new customer_Info("John", "Johnson", "jjohn@me.org", "657-902-3368");
 
 //Display email address for customer 1
-document.getElementById("cust").innerHTML = "The email address for the customer is: " + Cust1.email_Add;
-                                                
+document.getElementById("cust").innerHTML = "The email address for the customer is: " + Cust1.email;
+  
+//Creating a nested function to perform addition
+function Nested_Function()      {
+    document.getElementById("adding").innerHTML = add();
+    function add()      {
+        var Starting_value = 5;
+        function Add_one() {Starting_value += 1;}
+        Add_one();
+        return Starting_value;
+    }
+}
