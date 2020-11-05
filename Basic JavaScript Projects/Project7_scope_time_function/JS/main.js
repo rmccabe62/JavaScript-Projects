16e00e896ff1aa3bcd83a5444e48a3c5a5ddf993
rmccabe62 <72869399+rmccabe62@users.noreply.github.com>
2020-11-05 05:23:31 -0500

    updated files not working

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,55 @@
+var x = 8;
+function Subtract_numbers_1()    {
+document.write(40 - x + "<br>");
+}
+
+function Subtract_numbers_2()   {
+    document.write(25 - x + "<br>");
+}
+Subtract_numbers_1();
+Subtract_numbers_2();
+Subtract_numbers_3();
+Subtract_numbers_4();
+
+function Subtract_numbers_3()   {
+    var y = 2;
+    document.write(4 - y);
+}
+
+
+function Subtract_numbers_4()   {
+    document.write(x - y);
+}
+
+function get_Date()   {
+    if (new Date().getHours() < 20)     {
+        document.getElementById("time").innerHTML = "There is plenty of time left today!";
+    }
+
+}
+
+
+function Subtract_numbers_4()   {
+    console.log(y);
+}
+
+function my_Age()      {
+    var x = 58;
+        if (x > 50)     {
+            document.getElementById("age").innerHTML = "I am so old!";
+        }
+        function name_Saying()     {
+            if (name = "John")     {
+                document.getElementById("quoted").innerHTML = "Carpe Diem!";
+            }
+            else    {
+                document.write("Have a nice day!");
+            }
+        }
+}
+
+
+
+
+
+
