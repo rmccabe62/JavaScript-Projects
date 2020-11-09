0c3deb22b5695cfbd7bfbf5bc1dbc304d0da836f
rmccabe62 <72869399+rmccabe62@users.noreply.github.com>
2020-11-09 14:31:08 -0500

    updated files

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -27,7 +27,7 @@ function for_Loop()     {
 }
 
     var Tools = ["Hammer", "Wrench", "Screwdriver", "Saw"]      //Defining a list of variables in an array
-    var Item = "";      a                                         //Defining a variable to hold a value from list array
+    var Item = "";                                               //Defining a variable to hold a value from list array
     var y;                                                        //Defining a variable to be used as an index counter
 function array_Function()   {
     for (y = 0; y < Tools.length; y++)      {                   //Initiating a for loop to test if a condition is met
@@ -36,16 +36,16 @@ function array_Function()   {
     document.getElementById("Array").innerHTML = Item;             //Retreiving an element by id and writing a value from the list array to the screen           
 }
 function constant_function()    {
-    const Dog = {breed: "collie", color: "black", sex: "male"};
-    Dog.breed = "Shepard";
-    Dog.health = "good";
-    document.getElementById("Constant").innerHTML = "This dog's breed is a  " +Dog.breed  + " and his health is "
-        + Dog.health;
+    const Dog = {breed: "collie", color: "black", sex: "male"};    //Defining an object that is unchanging to the program
+    Dog.breed = "Shepard";                                         //Defining a property for the constant and assigning a value to it
+    Dog.health = "good";                                           //Defining a property for the constant and assigning a value to it
+    document.getElementById("Constant").innerHTML = "This dog's breed is a  " +Dog.breed  + " and his health is "       
+        + Dog.health;                                                   //Retreiving an element by id and writing a string for the result
 }
 
 function let_Keyword()      {
-    var a = 20;
-    document.write(a);
+    var a = 20;                                     //Defining a variable and assigning a value to it
+    document.write(a);                              //
     {
         let a = 40;
         document.write("<br" + a);
@@ -53,9 +53,52 @@ function let_Keyword()      {
     document.write("<br" + a);
 }
 
-var  x = ret_Stat(2,7);
-function ret_Stat(y,z)     {
-  return y + z;
+   
+function ret_Stat()     {
+    var y = 2;
+    var z = 7;
+    return y + z;
+
+    
+
 }
-document.getElementById("result").innerHTML = x;
+document.getElementById("result").innerHTML = ret_Stat();
+
+  let student = { 
+    studID: "A34D",
+    status: "Full-Time",
+    sex:    "female",
+    StudName: "Susan Baker",
+    description:function()  {
+      return "The student's name is " +  this.StudName + "and the student's status is " + this.status;    
+  }
+};
+document.getElementById("studentInfo").innerHTML = student.description();
+
+function breakStat()    {
+    var text = "";
+    var i;
+    for (i = 0; i < 11; i++)  {
+      if (i === 8) { break; }
+      text += "The number you want is " + i + "<br>";
+    document.getElementById("breaks").innerHTML = text; 
+    }
+}
+
+function continStat()   {
+    var text = "";
+    var i;
+    for (i = 0; i < 9; i++) {
+        if (i === 5) {continue;}
+        text += "Some numers are " + i + "<br>";
+    document.getElementById("go_On").innerHTML = text;
+    }
+}
+
+
+
+
+
+
+
 
