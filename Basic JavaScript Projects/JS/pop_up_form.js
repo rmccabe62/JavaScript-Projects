81c2b7c419ff4f1e01b0bb6797ff11328da4b9e7
rmccabe62 <72869399+rmccabe62@users.noreply.github.com>
2020-11-19 14:44:18 -0500

    updated files

---
+++ Basic JavaScript Projects/JS/pop_up_form.js
@@ -0,0 +1,9 @@
+//Opens the form to accept input from user
+function openForm() {
+    document.getElementById("popUp").style.display = "block";
+}
+
+//Closes the form after the user is done entering information
+function closeForm() {
+    document.getElementById("popUp").style.display = "none";
+}
