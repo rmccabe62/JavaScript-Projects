155b31ce12b8b9e66d9cea83199ceab3b7ceba27
rmccabe62 <72869399+rmccabe62@users.noreply.github.com>
2020-11-20 19:04:23 -0500

    update files

---
+++ Basic JavaScript Projects/JS/sticky_elem.js
@@ -0,0 +1,9 @@
+//Defining a function to open the form
+function openForm() {
+    document.getElementById("stickyForm").style.display = "block";
+}
+
+//Defining a function to close the form
+function closeForm() {
+    document.getElementById("stickyForm").style.display = "none";
+}
